2ebdb6963ea892c031af3d14ffe7fdaa0a7b6282
sourceperl <loic.celine@free.fr>
2016-05-30 16:43:45 +0200

    now use pyModbusTCP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pymbserver
 A python multithreaded modbus/TCP server (support function 1, 2, 3, 4, 5, 6, 15 and 16)
+
+New: all server code is now include in pyModbusTCP package (https://github.com/sourceperl/pyModbusTCP)
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-    packages=['pyModbusTCPServer'],
+    name='pymbserver',
+    description='A modbus TCP server',
+    long_description='',
+    author='Loic Lefebvre',
+    author_email='loic.celine@free.fr',
+    license='MIT',
+    url='https://github.com/sourceperl/pymbserver',
     platforms='any',
+    install_requires=required,
     scripts=[
         'scripts/pymbserver'
     ]
